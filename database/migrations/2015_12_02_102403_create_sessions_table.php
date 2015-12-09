ca86565b567c213645a23f2e033af143d0126c41
Kaguya <kaguya@ganxiang.wang>
2015-12-09 20:27:28 +0800

    完成基本功能

---
+++ database/migrations/2015_12_02_102403_create_sessions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sessions', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('session_fp');
+            $table->text('user_pub');
+            $table->text('verify_string');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('sessions');
+	}
+
+}
